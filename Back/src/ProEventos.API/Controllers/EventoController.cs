c286e65030d71952dd9cd4449928d78f3ef79a2c
Brenno Vasconcellos <br4nnogsvascon@gmail.com>
2026-04-09 21:35:50 -0300

    Comecei a criar a API, e já adicionei o controller de eventos.

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        { 
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento[] {
+            
+                new Evento()
+                {
+                    EventoId = 1,
+                    Tema = "Angular e .NET",
+                    Local = "Belo Horizonte",
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),  
+                    QtdPessoas = 250,
+                    Lote = "1º Lote",
+                    ImagemURL = "foto.png"
+                },
+                new Evento()
+                {
+                    EventoId = 2,  
+                    Tema = "Angular e .NET",
+                    Local = "Sao Paulo",
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),  
+                    QtdPessoas = 350,
+                    Lote = "1º Lote",
+                    ImagemURL = "foto.png"
+                }
+            };
+        }
+    }
+}
