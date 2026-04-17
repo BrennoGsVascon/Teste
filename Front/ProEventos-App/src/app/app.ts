9f24c631953d97a7adaeb527733282aa159f2c4d
Brenno Vasconcellos <br4nnogsvascon@gmail.com>
2026-04-17 16:59:35 -0300

    Correçao do Ngfor nao aplicado nos componentes

--- Front/ProEventos-App/src/app/app.ts
+++ Front/ProEventos-App/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Eventos } from "./eventos/eventos";
 import { Palestrantes } from "./palestrantes/palestrantes";
 import { NavComponent } from "./nav/Nav.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Eventos, Palestrantes, NavComponent],
+  imports: [RouterOutlet, Eventos, Palestrantes, NavComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- Front/ProEventos-App/src/app/eventos/eventos.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-eventos',
-  templateUrl: './eventos.html',
+  standalone: true,
   imports: [CommonModule],
+  templateUrl: './eventos.html',
   styleUrl: './eventos.scss',
 })
 export class Eventos {
